c2c09137541e46dc1051a73ee5a238ad39ca97e4
Сергей Бердюг <sberdyug@gmail.com>
2017-05-03 02:41:17 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,21 +1,21 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("https://www.pornhub.com/playlists?page=16")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+root.cssselect("ul.videos")
 #
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+scraperwiki.sql.select("* from data where 'name'='peter'")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
@@ -23,9 +23,3 @@
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
 
-import requests
-user_id = 12345
-url = 'http://www.kinopoisk.ru/user/%d/votes/list/ord/date/page/2/#list' % (user_id) # url для второй страницы
-r = requests.get(url)
-with open('test.html', 'w') as output_file:
-  output_file.write(r.text.encode('cp1251'))
